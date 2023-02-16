c6c3c781075d49dec3af81e791bbd78b1f5967e0
darshanStha777 <darsun.shrestha@gmail.com>
2023-02-16 13:39:58 +0545

    added module

--- controller/authController.js
+++ controller/authController.js
@@ -1,25 +1,16 @@
+import RegisterModel from "../modules/registrationForm.js";
 class AuthController {
-
-    // Login Controller Starts from here
-    static NewAccount = async (req, res) => {
-
-        try {
-            
-            // getting the information from the user
-
-            // validation of the user
-
-            // check the user if aready exist in the database
-
-
-
-        } catch (error) {
-            console.log(error)
-        }
-
+  // Login Controller Starts from here
+  static NewAccount = async (req, res) => {
+    try {
+      // getting the information from the user
+      // validation of the user
+      // check the user if aready exist in the database
+    } catch (error) {
+      console.log(error);
     }
-    // Login Controller Ends here
-
+  };
+  // Login Controller Ends here
 }
 
-export default AuthController
+export default AuthController;
